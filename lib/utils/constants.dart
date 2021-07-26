004de0b2cd17b60599397a9b3ca20dba235c4c52
Anton Sh <poshllust@gmail.com>
2021-07-27 00:36:48 +0300

    *instructions url

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
   static final String RULES_URL = 'https://imes.pro/consent_to_research.html';
   static final String POLICY_URL = 'https://imes.pro/policy.html';
-  static final String INSTRUCTION_URL = 'https://imes.pro/instruction.png';
+  static final String INSTRUCTION_URL = 'https://imes.pro/storage/app/uploads/instruction.png';
 
   static final String FAVORITES_BOX = 'favorite';
 }
